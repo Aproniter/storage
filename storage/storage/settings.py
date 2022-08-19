ffeed622aef1573ca9a84225c9e7e3abe7dfc42e
Aproniter <ivan.belokrylov@gmail.com>
2022-08-19 19:09:39 +0300

    add celery

--- storage/storage/settings.py
+++ storage/storage/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
